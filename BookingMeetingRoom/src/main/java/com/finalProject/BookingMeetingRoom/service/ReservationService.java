8e86134b3366ee31f6441cf5966540d5a3e85bac
Nghiem Xuan Tinh <tinhnxhe170518@fpt.edu.vn>
2026-03-06 16:09:56 +0700

    add extend room and return room

--- BookingMeetingRoom/src/main/java/com/finalProject/BookingMeetingRoom/service/ReservationService.java
+++ BookingMeetingRoom/src/main/java/com/finalProject/BookingMeetingRoom/service/ReservationService.java
@@ -10,12 +10,20 @@ import org.springframework.security.core.Authentication;
 import java.util.List;
 
 public interface ReservationService {
-   
 
     void checkIn(String reservationId, Authentication authentication);
+
     void cancelReservation(String reservationId, Authentication connectedUser);
+
     ReservationResponse reserveRoom(ReservationRequest request, Authentication connectedUser);
+
     Page<ReservationResponse> getAllReservations(int page, int size);
-    Page<MyReservationResponse> getReservationStatus(int page, int size, Authentication connectedUser, String locationCode, String address, List<String> statuses,
-                                                     String buildingId, String startTime, String endTime);
+
+    Page<MyReservationResponse> getReservationStatus(int page, int size, Authentication connectedUser,
+            String locationCode, String address, List<String> statuses,
+            String buildingId, String startTime, String endTime);
+
+    void extendReservation(String reservationId, double hour, Authentication connectedUser);
+
+     void returnRoom(String reservationId, Authentication authentication);
 }
